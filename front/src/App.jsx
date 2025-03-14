90cbac89165acda13a66f36b15df9e34e29073b8
satyajit-patel <satyajitpatel770001@gmail.com>
2025-03-14 13:34:38 +0530

    bug fix. stopped listening when TTS is playing

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import { GoogleGeminiEffectDemo } from "./components/BackgroundLines/GoogleGemin
 import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import NewHome from "./components/Home/NewHome"
 
 const AppContent = () => {
   const location = useLocation();
@@ -11,7 +12,8 @@ const AppContent = () => {
       {location.pathname !== "/" && <Navbar />}
       <Routes>
         <Route path="/" element={<GoogleGeminiEffectDemo />} />
-        <Route path="/Home" element={<Home />} />
+        {/* <Route path="/Home" element={<Home />} /> */}
+        <Route path="/Home" element={<NewHome />} />
       </Routes>
     </>
   );
